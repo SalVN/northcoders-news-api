b46a205cdfec470e2ea63e1573f7f2a4a6244480
SalVN <sal.v.newell@gmail.com>
2017-09-12 12:52:20 +0100

    change url images in seed database

--- seed/data/user_data.js
+++ seed/data/user_data.js
@@ -1,8 +1,8 @@
 module.exports = [
-  {username: 'tickle122', name: 'Tom Tickle', avatar_url: 'http://www.spiritsurfers.net/monastery/wp-content/uploads/_41500270_mrtickle.jpg'},
-  {username: 'grumpy19', name: 'Paul Grump', avatar_url: 'http://www.tumbit.com/profile-image/4/original/mr-grumpy.jpg'},
+  {username: 'tickle122', name: 'Tom Tickle', avatar_url: 'http://res.cloudinary.com/dl37xtqhv/image/upload/c_pad,h_200,w_200/v1505216852/tickle.jpg'},
+  {username: 'grumpy19', name: 'Paul Grump', avatar_url: 'http://res.cloudinary.com/dl37xtqhv/image/upload/c_pad,h_200,w_200/v1505216751/grumpy.gif'},
   {username: 'happyamy2016', name: 'Amy Happy', avatar_url: 'http://res.cloudinary.com/dl37xtqhv/image/upload/v1504883423/sunshine.jpg'},
   {username: 'cooljmessy', name: 'Peter Messy', avatar_url: 'http://res.cloudinary.com/dl37xtqhv/image/upload/v1504883425/messy.jpg'},
   {username: 'weegembump', name: 'Gemma Bump', avatar_url: 'http://res.cloudinary.com/dl37xtqhv/image/upload/v1504883421/bump.jpg'},
-  {username: 'jessjelly', name: 'Jess Jelly', avatar_url: 'https://s-media-cache-ak0.pinimg.com/564x/39/62/ec/3962eca164e60cf46f979c1f57d4078b.jpg'}
+  {username: 'jessjelly', name: 'Jess Jelly', avatar_url: 'http://res.cloudinary.com/dl37xtqhv/image/upload/v1505216747/jelly.jpg'}
 ];
